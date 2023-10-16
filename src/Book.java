e0d8927ab341669890a568429ca7d00eb32c365c
Inv <artem@tarabakin.ru>
2023-10-16 17:39:59 +0300

    Done

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Book {
+public class Book {
+    private String bookTitle;
+    private int yearPublication;
+    private Author author;
+
+    @Override
+    public String toString() {
+        return
+                "Название книги: " + bookTitle +
+                ", Год публикации: " + yearPublication +
+                ", Автор: " + author;
+    }
+
+    public Book(String bookTitle, int yearPublication, Author author) {
+        this.bookTitle = bookTitle;
+        this.yearPublication = yearPublication;
+        this.author = author;
+    }
+
+    public void setYearPublication(int yearPublication) {
+        this.yearPublication = yearPublication;
+    }
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public int getYearPublication() {
+        return yearPublication;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Author author = new Author("Юваль Ной"," Харари");
+        Author author2 = new Author("Джордж", "Оруэлл");
+        Book book = new Book("Sapiens. Краткая история человечества",2014,author);
+        Book book2 = new Book("1984",1949,author2);
+
+        System.out.println(book);
+        System.out.println(book2);
+        book2.setYearPublication(2023);
+        System.out.println(book2);
+
+
+    }
 }
